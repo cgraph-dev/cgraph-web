9b12d489e825a4b24b9f62fd3d9eb05809335949
cgraph-dev <contact@cgraph.org>
2026-07-29 11:52:14 +0300

    refactor(ui): remove dead avatar settings module

--- apps/web/src/modules/settings/components/avatar-settings/constants.ts
+++
@@ -1,8 +0,0 @@
-/**
- * AvatarSettings constants
- */
-
-export const MAX_BIO_LENGTH = 500;
-export const MAX_LOCATION_LENGTH = 100;
-export const MAX_OCCUPATION_LENGTH = 100;
-export const MAX_AVATAR_SIZE_MB = 2;
